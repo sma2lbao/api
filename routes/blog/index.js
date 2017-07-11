b1270fea5bfcfda8adc613bfd3f0e89719bdb7e5
Sma2lBao Lee <sma2lbaolee@Sma2lBaodeMacBook-Pro.local>
2017-07-12 00:13:41 +0800

    update write article

--- routes/blog/index.js
+++ routes/blog/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var multiparty = require('multiparty');
 var router = express.Router();
+var file = require('fs');
+var path =  require('path')
+var typeList = ['主页', '生活记录', '工作记录', '心情随笔']
 
 router.get('/', function (req, res, next) {
   res.send({msg: 'success'})
@@ -18,8 +21,29 @@ router.get('/user', function (req, res, next) {
 
 router.get('/heads', function (req, res, next) {
   res.send({
-    'data': ['主页', '生活记录', '工作记录', '心情随笔']
+    'data': typeList
   })
+})
+
+router.post('/article', function (req, res, next) {
+  console.log(JSON.parse);
+  req.body.comt = JSON.stringify(req.body.comt)
+  var articleComt = '{"comt": '+ req.body.comt +'}';
+  file.writeFile(path.resolve(__dirname, '../article', typeList[req.body.index] + '/', req.body.title+ '_' + new Date().getTime() +'.json'), articleComt, function (err) {
+    if(err) {
+      console.log(err);
+      res.send({
+        error: '保存失败'
+      })
+    }
+    else {
+      console.log('save success');
+      res.send({
+        msg: '保存成功'
+      })
+    }
+  })
+
 })
 
 module.exports = router;
