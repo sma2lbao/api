c949f46e56aa34af0b4c28bb91a528c3dda26f53
sma2lbao <sma2lbao@qq.com>
2017-07-11 21:01:47 +0800

    'update'

--- routes/blog/index.js
+++ routes/blog/index.js
@@ -9,11 +9,17 @@ router.get('/', function (req, res, next) {
 router.get('/user', function (req, res, next) {
   const user = req.query.user;
   const pass = req.query.pass;
-  if('sma2lbal' === user && '000000' === pass){
-    res.send({msg: 'success'})
+  if('sma2lbao' === user && '000000' === pass){
+    res.send({msg: '登录成功'})
   } else {
-    res.send({error: 'error'})
+    res.send({error: '登录失败'})
   }
+})
+
+router.get('/heads', function (req, res, next) {
+  res.send({
+    'data': ['主页', '生活记录', '工作记录', '心情随笔']
+  })
 })
 
 module.exports = router;
